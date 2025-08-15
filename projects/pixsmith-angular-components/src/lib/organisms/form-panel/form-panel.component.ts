5b50af11a7fdc2d85656a6cbdac729abdd17c63a
Jonathan Thibault <Jothib@outlook.com>
2025-08-14 21:17:18 -0400

    generated a couple components

---
+++ projects/pixsmith-angular-components/src/lib/organisms/form-panel/form-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-form-panel',
+  imports: [],
+  templateUrl: './form-panel.component.html',
+  styleUrl: './form-panel.component.css'
+})
+export class FormPanelComponent {
+
+}
